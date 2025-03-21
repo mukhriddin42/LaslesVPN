be25fc429278f36e6c084e7fc2a8c6bcc27c4041
mukhriddin03 <imuhriddin700@gmail.com>
2025-03-21 21:48:02 +0500

    night mode

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,41 @@ window.addEventListener("scroll", function () {
 backToTopButton.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" }); // Silliq scroll yuqoriga
 });
+
+
+
+
+let body = document.querySelector("body");
+let night_mode_btn = document.querySelector(".night_mode_link");
+let sun_icon = '<i class="fa-solid fa-sun"></i>';
+let moon_icon = '<i class="fa-solid fa-moon"></i>'
+
+function changeMode() {
+    if(night_mode_btn.innerHTML === moon_icon){
+        night_mode_btn.innerHTML = sun_icon;
+        body.style.backgroundColor = "#000000";
+    } else if (night_mode_btn.innerHTML === sun_icon) {
+        night_mode_btn.innerHTML = moon_icon;
+        body.style.backgroundColor = "#ffffff"
+    }
+    
+}
+
+
+
+let modal = document.getElementById("myModal");
+let closeBtn = document.getElementById("closeModal");
+function openModal() {
+    console.log(modal);
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+window.addEventListener("click", function (e) {
+    if(e.target === modal) {
+        modal.style.display = "none";
+    }
+})
